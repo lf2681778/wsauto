33fe1f31132ce01b0ab3e0c1cc93d4de05aedc1e
lf2681778 <33818706+lf2681778@users.noreply.github.com>
2026-08-27 10:54:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ async function updateVendor(token, vendorId) {
 
 async function main() {
     const token = await getToken();
-    const workbook = XLSX.readFile('VendorWatsons');
+    const workbook = XLSX.readFile('VendorWatsons.xlsx');
     const sheet = workbook.Sheets[workbook.SheetNames[0]];
     const rawData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
     
